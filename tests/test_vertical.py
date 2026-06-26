82f2216ddc003ed3a0672611d1d2ccdf6fcd76f0
Supriya Rajendra Morankar <supriyamorankar@gmail.com>
2026-06-26 16:51:44 +0530

    remove verticle

--- tests/test_vertical.py
+++ tests/test_vertical.py
@@ -8,9 +8,9 @@ def test_trading_options_click(page):
     vertical_page = vertical(page)
     vertical_page.trading_options_click()
 
-def test_retail_and_ecommerce_options_click(page):
-    vertical_page = vertical(page)
-    vertical_page.retail_and_ecommerce_options_click()
+# def test_retail_and_ecommerce_options_click(page):
+#     vertical_page = vertical(page)
+#     vertical_page.retail_and_ecommerce_options_click()
 
 def test_healthcare_options_click(page):
     vertical_page = vertical(page)
