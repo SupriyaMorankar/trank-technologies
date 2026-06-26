98e39337f1ba2fe9267e2b0250a4ef36454bf9e1
Supriya Rajendra Morankar <supriyamorankar@gmail.com>
2026-06-26 15:58:50 +0530

    remove footer option

--- tests/test_footers.py
+++ tests/test_footers.py
@@ -7,9 +7,9 @@ def test_footer_options_click(page):
     footer = footers(page)
     footer.footer_options_click()
 
-def test_ui_ux_options_click(page):
-    footer = footers(page)
-    footer.ui_ux_options_click()
+# def test_ui_ux_options_click(page):
+#     footer = footers(page)
+#     footer.ui_ux_options_click()
 
 def test_app_development_options_click(page):
     footer = footers(page)
